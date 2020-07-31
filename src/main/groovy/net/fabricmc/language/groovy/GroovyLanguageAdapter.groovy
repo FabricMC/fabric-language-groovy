ba13dc8c499adbd26831445626b89bc7a73a2961
TheSledgeHammer <martin.kelly4000@gmail.com>
2020-07-31 18:14:03 +1000

    Initial Release for Minecraft 1.16.1 - Updates Groovy & Gradle versions
    Signed-off-by: TheSledgeHammer <martin.kelly4000@gmail.com>

--- src/main/groovy/net/fabricmc/language/groovy/GroovyLanguageAdapter.groovy
+++ src/main/groovy/net/fabricmc/language/groovy/GroovyLanguageAdapter.groovy
@@ -9,13 +9,13 @@ import org.apache.logging.log4j.Logger
 
 class GroovyLanguageAdapter implements LanguageAdapter  {
 
-    private Logger LOGGER = LogManager.getFormatterLogger("GroovyLanguageAdapter");
+    private Logger LOGGER = LogManager.getFormatterLogger("GroovyLanguageAdapter")
 
     @Override
-    def <T> T create(ModContainer modContainer, String s, Class<T> aClass) throws LanguageAdapterException {
-        Class groovyClass = Class.forName(aClass.getName());
+    <T> T create(ModContainer modContainer, String s, Class<T> aClass) throws LanguageAdapterException {
+        Class groovyClass = Class.forName(aClass.getName())
         if(groovyClass == null) {
-            LOGGER.debug("Unable to find Groovy object instance for ${aClass.name}, constructing new instance");
+            LOGGER.debug("Unable to find Groovy object instance for ${aClass.name}, constructing new instance")
         }
         return DefaultLanguageAdapter.INSTANCE.create(modContainer, s, aClass)
     }
